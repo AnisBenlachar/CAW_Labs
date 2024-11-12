24dc2cada13c3f703f7e94f95ae78fa818807a30
Benlachar Anis <nissou5353@gmail.com>
2024-11-12 19:57:39 +0100

    first commit of the Bonus Lab

---
+++ BonusLab/exo1.js
@@ -0,0 +1,13 @@
+function swap(a,b){
+    b = [a, a = b][0];
+}
+
+function arrayConcat(arr1,arr2,arr3){
+    const arr4 = arr1.concat(arr2,arr3)
+    console.log(arr4);
+}
+function arrspread (str){
+    const str1= [...str]
+    console.log(str1)
+}
+
